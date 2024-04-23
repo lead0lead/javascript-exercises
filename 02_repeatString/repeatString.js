742bfd7485940c142cf9cf4eb940ef945241db59
leader0leader <leander.hemmelskamp@proton.me>
2024-04-23 21:53:42 +0200

    Complete 01_helloWorld and 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,21 @@
-const repeatString = function() {
+const repeatString = function(sString, nNum) {
 
-};
+    let stringChain = "";
+
+        for (let i = 0; i < nNum; i++){
+            if (i === 0){
+            stringChain = sString;
+            }
+            else{
+            stringChain = stringChain + sString;
+            }
+    } 
+    if (nNum < 0){
+        return "ERROR";
+    }
+    else
+        return(stringChain);
+}
 
 // Do not edit below this line
 module.exports = repeatString;
