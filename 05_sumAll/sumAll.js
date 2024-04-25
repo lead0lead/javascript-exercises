af3e8c6542d49ee5fd999fbf74f4ea50a98391a7
leader0leader <leander.hemmelskamp@proton.me>
2024-04-25 21:58:56 +0200

    Complete 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,29 @@
-const sumAll = function() {
+const sumAll = function(firstInt, secondInt) {
 
+    if (firstInt < 0 ||
+        secondInt < 0 || 
+        typeof firstInt != "number" ||
+        typeof secondInt != "number"){
+        return "ERROR";
+    }
+
+    let smallInt = firstInt;
+    let bigInt = secondInt;
+
+    if (firstInt > secondInt){
+        smallInt = secondInt;
+        bigInt = firstInt;
+    }
+
+    let sum = smallInt;
+
+    for (let i = smallInt; i <= bigInt; i++){
+        if (i > smallInt){
+            sum = sum + i;
+        }
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
