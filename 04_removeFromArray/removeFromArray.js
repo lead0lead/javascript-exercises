195bc5e84459288d3cc03323f06b99b17153cfc1
leader0leader <leander.hemmelskamp@proton.me>
2024-04-25 21:27:32 +0200

    Complete 03_reverseString and 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,18 @@
 const removeFromArray = function() {
 
+    let aArray = arguments[0];
+
+    for (i = 1; i < arguments.length; i++){
+        for (j = 0; j < aArray.length; j++){
+            if (aArray[j] === arguments[i]){
+                aArray.splice(j, 1);
+                j = j - 1;
+            }
+        }
+    }
+
+    return aArray;
+
 };
 
 // Do not edit below this line
